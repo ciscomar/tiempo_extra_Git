266762430d65fe48bf1f699eaa9eaa37b0b6b545
ciscomar <cisco.morales.27@gmail.com>
2023-06-02 16:17:07 -0600

    update

--- routes/routesController.js
+++ routes/routesController.js
@@ -1605,13 +1605,11 @@ controller.getSolicitudesAprobar_POST = (req, res) => {
         }
 
 
-        setTimeout(() => {
             result.push(allEmpleados)
             result.push(solicitudesConf)
             result.push(sumahorasConf)
             res.json(result)
-    
-          }, 3000);
+
 
 
 
